c362f95aee9286ebf4c4e59c0df8e3db8ec9f6f5
Evgeny Borzenin <evgeny.borzenin@vipps.no>
2020-02-03 23:27:40 +0100

    remaned build to publish

--- events/01-immutable-infrastructure-on-azure-in-action/src/api/deploy.sh
+++ events/01-immutable-infrastructure-on-azure-in-action/src/api/deploy.sh
@@ -4,5 +4,5 @@ environment=$1
 resourceGroupName="iac-$environment-rg"
 apiWebAppName="iac-$environment-api-appservice"
 
-./build.sh
+publish.sh
 az webapp deployment source config-zip -g ${resourceGroupName} -n ${apiWebAppName} --src "./api.zip"
--- events/01-immutable-infrastructure-on-azure-in-action/src/api/publish.sh
+++ events/01-immutable-infrastructure-on-azure-in-action/src/api/publish.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 
 rm -r ./publish
-rm ./api.zip
 dotnet publish ../api.sln -c Release -o ./publish
-cd ./publish
-zip -r ../api.zip .
-cd ..
