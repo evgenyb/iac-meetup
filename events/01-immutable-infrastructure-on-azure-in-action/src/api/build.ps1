fa2ee6063514d8e3b9a1a6befafcc53c1604a7de
Evgeny Borzenin <evgeny.borzenin@vipps.no>
2020-02-03 14:43:12 +0100

    Fixed build script

--- events/01-immutable-infrastructure-on-azure-in-action/src/api/build.ps1
+++ events/01-immutable-infrastructure-on-azure-in-action/src/api/build.ps1
@@ -2,8 +2,13 @@ Param(
     [string] [Parameter(Mandatory = $true)] $environment
 )
 
-Remove-Item api.zip -Force
-& "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\amd64\msbuild.exe" "..\api.sln" /nologo /nr:false /m /p:DeployOnBuild=true /p:PublishProfile=publish-local
-Compress-Archive -Path ".\publish\*" -DestinationPath api.zip -Force
-$resourceGroupName = 'iac-' + $environment + '-rg'
-$apiWebAppName = 'iac-' + $environment + '-api-appservice'
+#Remove-Item api.zip -Force
+$solutionFile = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, "..\api.sln"))
+
+
+& "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\amd64\msbuild.exe" $solutionFile /nologo /nr:false /m /p:DeployOnBuild=true /p:PublishProfile=publish-local
+$publishFolder = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, "publish"))
+$publishFolder = $publishFolder + "\*"
+Write-Output $publishFolder
+
+Compress-Archive -Path $publishFolder -DestinationPath api.zip -Force
