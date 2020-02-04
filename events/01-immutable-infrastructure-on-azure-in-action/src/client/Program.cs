5ad4e13fb50fc5397ff6da6e4b372339dc1acab7
Evgeny Borzenin <evgeny.borzenin@vipps.no>
2020-02-04 22:19:09 +0100

    Changed AGW ARM, added client

---
+++ events/01-immutable-infrastructure-on-azure-in-action/src/client/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace client
+{
+    class Program
+    {
+        static readonly HttpClient client = new HttpClient();
+        static TelemetryConfiguration configuration = TelemetryConfiguration.CreateDefault();
+
+        static async Task Main()
+        {
+            configuration.InstrumentationKey = "a6b2e925-97b5-445f-942a-4bdb9f125c32";
+            var telemetryClient = new TelemetryClient(configuration);
+            try
+            {
+                telemetryClient.TrackTrace("Calling http://iac-dev-api-appservice.azurewebsites.net/values",
+                    SeverityLevel.Information);
+                var response = await client.GetAsync("http://iac-dev-api-appservice.azurewebsites.net/values");
+                response.EnsureSuccessStatusCode();
+                var responseBody = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(responseBody);
+            }
+            catch (HttpRequestException e)
+            {
+                telemetryClient.TrackException(e);
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
+            finally
+            {
+                telemetryClient.Flush();
+            }
+        }
+    }
+}
