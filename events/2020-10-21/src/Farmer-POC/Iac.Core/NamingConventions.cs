af5f59c59b8fa870f357ded6fd4078ac8708e3a9
Evgeny Borzenin <evgeny.borzenin@gmail.com>
2020-10-22 00:13:34 +0200

    Even 21.10 materials

---
+++ events/2020-10-21/src/Farmer-POC/Iac.Core/NamingConventions.cs
@@ -0,0 +1,28 @@
+﻿namespace IacCore
+{
+    
+    public static class NamingConventions
+    {
+        const string Prefix = "iac";
+        
+        public static string GetResourceGroupName(string environment)
+        {
+            return $"{Prefix}-{environment}-rg";
+        }
+        
+        public static string GetStorageAccountName(string saType, string environment)
+        {
+            return $"{Prefix}{environment}{saType}sa";
+        }
+        
+        public static string GetFunctionName(string funcName, string environment)
+        {
+            return $"{Prefix}-{environment}-{funcName}-func";
+        } 
+        
+        public static string GetServicePlanName(string environment)
+        {
+            return $"{Prefix}-{environment}-sp";
+        }
+    }
+}
